dddad7cd731ad0058dddbcd6817b6c727d7469a6
Peng Du <pdu@nvidia.com>
2013-07-15 20:40:52 -0700

    ARM: tegra: move cpu-based timers out of timer.c
    This change cleans up timer.c by pulling out cpu-based timers
    (ie. arch & twd) into their own files. timer.c now hosts code
    only for the Tegra SOC timers and common init code.
    
    Also relocated deep power down (DPD) code from timer.c to pm.c
    so that it can be shared by other chip family.
    
    Change-Id: I247e514270e5a81a5eb1bde6d7f75692cbede1f0
    Signed-off-by: Peng Du <pdu@nvidia.com>
    Reviewed-on: http://git-master/r/250353
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: Prashant Gaikwad <pgaikwad@nvidia.com>
    Signed-off-by: Mourta P <mourta.nova@gmail.com>

--- arch/arm/mach-tegra/timer.c
+++ arch/arm/mach-tegra/timer.c
@@ -30,22 +30,17 @@
 #include <linux/cpu.h>
 #include <linux/io.h>
 #include <linux/syscore_ops.h>
-#include <linux/cpu_pm.h>
 #include <linux/of.h>
 #include <linux/rtc.h>
 #include <asm/localtimer.h>
 
 #include <asm/mach/time.h>
-#include <asm/arch_timer.h>
-#include <asm/cputype.h>
-#include <asm/smp_twd.h>
-#include <asm/system.h>
+#include <asm/delay.h>
 #include <asm/sched_clock.h>
+#include <asm/localtimer.h>
 
 #include <mach/iomap.h>
 #include <mach/io.h>
-#include <mach/irqs.h>
-#include <mach/hardware.h>
 
 #include "board.h"
 #include "clock.h"
@@ -55,11 +50,9 @@
 static void __iomem *timer_reg_base = IO_ADDRESS(TEGRA_TMR1_BASE);
 static void __iomem *rtc_base = IO_ADDRESS(TEGRA_RTC_BASE);
 
-#ifdef CONFIG_ARM_ARCH_TIMER
-static u32 arch_timer_us_mult, arch_timer_us_shift;
-#endif
 static u64 persistent_ms, last_persistent_ms;
 static struct timespec persistent_ts;
+
 static u32 usec_config;
 
 #ifdef CONFIG_ARCH_TEGRA_2x_SOC
@@ -237,7 +230,7 @@ u64 tegra_rtc_read_ms(void)
 }
 
 /*
- * read_persistent_clock -  Return time from a persistent clock.
+ * tegra_read_persistent_clock -  Return time from a persistent clock.
  *
  * Reads the time from a source which isn't disabled during PM, the
  * 32k sync timer.  Convert the cycles elapsed since last read into
@@ -246,7 +239,7 @@ u64 tegra_rtc_read_ms(void)
  * tegra_rtc driver could be executing to avoid race conditions
  * on the RTC shadow register
  */
-void read_persistent_clock(struct timespec *ts)
+void tegra_read_persistent_clock(struct timespec *ts)
 {
 	u64 delta;
 	struct timespec *tsp = &persistent_ts;
@@ -295,86 +288,7 @@ static struct syscore_ops tegra_timer_syscore_ops = {
 	.resume = tegra_timer_resume,
 };
 
-#ifdef CONFIG_HAVE_ARM_TWD
-static DEFINE_TWD_LOCAL_TIMER(twd_local_timer,
-			      TEGRA_ARM_PERIF_BASE + 0x600,
-			      IRQ_LOCALTIMER);
-static void __iomem *tegra_twd_base = IO_ADDRESS(TEGRA_ARM_PERIF_BASE + 0x600);
-
-void __init tegra_cpu_timer_init(void)
-{
-	struct clk *cpu, *twd_clk;
-	int err;
-
-	/* The twd clock is a detached child of the CPU complex clock.
-	   Force an update of the twd clock after DVFS has updated the
-	   CPU clock rate. */
-
-	twd_clk = tegra_get_clock_by_name("twd");
-	BUG_ON(!twd_clk);
-	cpu = tegra_get_clock_by_name("cpu");
-	err = clk_set_rate(twd_clk, clk_get_rate(cpu));
-
-	if (err)
-		pr_err("Failed to set twd clock rate: %d\n", err);
-	else
-		pr_debug("TWD clock rate: %ld\n", clk_get_rate(twd_clk));
-}
-
-int tegra_twd_get_state(struct tegra_twd_context *context)
-{
-	context->twd_ctrl = readl(tegra_twd_base + TWD_TIMER_CONTROL);
-	context->twd_load = readl(tegra_twd_base + TWD_TIMER_LOAD);
-	context->twd_cnt = readl(tegra_twd_base + TWD_TIMER_COUNTER);
-
-	return 0;
-}
-
-void tegra_twd_suspend(struct tegra_twd_context *context)
-{
-	context->twd_ctrl = readl(tegra_twd_base + TWD_TIMER_CONTROL);
-	context->twd_load = readl(tegra_twd_base + TWD_TIMER_LOAD);
-	if ((context->twd_load == 0) &&
-	    (context->twd_ctrl & TWD_TIMER_CONTROL_PERIODIC) &&
-	    (context->twd_ctrl & (TWD_TIMER_CONTROL_ENABLE |
-				  TWD_TIMER_CONTROL_IT_ENABLE))) {
-		WARN("%s: TWD enabled but counter was 0\n", __func__);
-		context->twd_load = 1;
-	}
-	__raw_writel(0, tegra_twd_base + TWD_TIMER_CONTROL);
-}
-
-void tegra_twd_resume(struct tegra_twd_context *context)
-{
-	BUG_ON((context->twd_load == 0) &&
-	       (context->twd_ctrl & TWD_TIMER_CONTROL_PERIODIC) &&
-	       (context->twd_ctrl & (TWD_TIMER_CONTROL_ENABLE |
-				     TWD_TIMER_CONTROL_IT_ENABLE)));
-	writel(context->twd_load, tegra_twd_base + TWD_TIMER_LOAD);
-	writel(context->twd_ctrl, tegra_twd_base + TWD_TIMER_CONTROL);
-}
-
-static void __init tegra_init_late_timer(void)
-{
-	int err;
-
-	if (of_have_populated_dt()) {
-		twd_local_timer_of_register();
-		return;
-	}
-
-	err = twd_local_timer_register(&twd_local_timer);
-	if (err)
-		pr_err("twd_timer_register failed %d\n", err);
-}
-#else /* !CONFIG_HAVE_ARM_TWD */
-#define tegra_twd_get_state	do {} while(0)
-#define tegra_twd_suspend	do {} while(0)
-#define tegra_twd_resume	do {} while(0)
-void __init tegra_cpu_timer_init(void) {}
-static void __init tegra_init_late_timer(void) {}
-
-#ifndef CONFIG_ARM_ARCH_TIMER
+#if !defined(CONFIG_HAVE_ARM_TWD) && !defined(CONFIG_ARM_ARCH_TIMER)
 
 static DEFINE_PER_CPU(struct tegra_clock_event_device, percpu_tegra_timer);
 
@@ -508,339 +422,15 @@ void tegra3_lp2_timer_cancel_secondary(void)
 	/* Nothing needs to be done here */
 }
 
-static void __init tegra_init_late_timer(void)
+void __init tegra_init_late_timer(void)
 {
 	local_timer_register(&tegra_local_timer_ops);
 }
-#endif /* CONFIG_ARM_ARCH_TIMER */
-#endif /* CONFIG_HAVE_ARM_TWD */
+#endif /* !CONFIG_HAVE_ARM_TWD && !CONFIG_ARM_ARCH_TIMER */
 
-#ifdef CONFIG_ARM_ARCH_TIMER
-
-static bool arch_timer_initialized;
-
-#ifdef CONFIG_TEGRA_PRE_SILICON_SUPPORT
-#ifndef CONFIG_TRUSTED_FOUNDATIONS
-/* Time Stamp Counter (TSC) base address */
-static void __iomem *tsc = IO_ADDRESS(TEGRA_TSC_BASE);
-#endif
-
-#define TSC_CNTCR		0		/* TSC control registers */
-#define TSC_CNTCR_ENABLE	(1 << 0)	/* Enable*/
-#define TSC_CNTCR_HDBG		(1 << 1)	/* Halt on debug */
-
-#define TSC_CNTCV0		0x8		/* TSC counter (LSW) */
-#define TSC_CNTCV1		0xC		/* TSC counter (MSW) */
-#define TSC_CNTFID0		0x20		/* TSC freq id 0 */
-
-#define tsc_writel(value, reg) \
-	__raw_writel(value, (u32)tsc + (reg))
-#define tsc_readl(reg) \
-	__raw_readl((u32)tsc + (reg))
-#endif /* CONFIG_TEGRA_PRE_SILICON_SUPPORT */
-
-/* Is the optional system timer available? */
-static int local_timer_is_architected(void)
-{
-	return (cpu_architecture() >= CPU_ARCH_ARMv7) &&
-	       ((read_cpuid_ext(CPUID_EXT_PFR1) >> 16) & 0xf) == 1;
-}
-
-void __init tegra_cpu_timer_init(void)
-{
-	u32 tsc_ref_freq;
-#ifdef CONFIG_TEGRA_PRE_SILICON_SUPPORT
-	u32 reg;
-#endif
-
-	if (!local_timer_is_architected())
-		return;
-
-	tsc_ref_freq = tegra_clk_measure_input_freq();
-	if (tsc_ref_freq == 115200 || tsc_ref_freq == 230400) {
-		/*
-		 * OSC detection function will bug out if revision is not
-		 * QT and the detected frequency is one of these two.
-		 */
-		tsc_ref_freq = 13000000;
-		pr_info("fake tsc_ref_req=%d in QT\n", tsc_ref_freq);
-	}
-
-#ifdef CONFIG_TEGRA_PRE_SILICON_SUPPORT
-	if (tegra_platform_is_linsim()) {
-		/* Set the Timer System Counter (TSC) reference frequency
-		   NOTE: this is a write once register */
-		tsc_writel(tsc_ref_freq, TSC_CNTFID0);
-
-		/* Program CNTFRQ to the same value.
-		   NOTE: this is a write once (per CPU reset) register. */
-		__asm__("mcr p15, 0, %0, c14, c0, 0\n" : : "r" (tsc_ref_freq));
-
-		/* CNTFRQ must agree with the TSC reference frequency. */
-		__asm__("mrc p15, 0, %0, c14, c0, 0\n" : "=r" (reg));
-		BUG_ON(reg != tsc_ref_freq);
-
-		/* Enable the TSC. */
-		reg = tsc_readl(TSC_CNTCR);
-		reg |= TSC_CNTCR_ENABLE | TSC_CNTCR_HDBG;
-		tsc_writel(reg, TSC_CNTCR);
-	}
-#endif
-	clocks_calc_mult_shift(&arch_timer_us_mult, &arch_timer_us_shift,
-				tsc_ref_freq, USEC_PER_SEC, 0);
-	return;
-}
-
-static void tegra_arch_timer_per_cpu_init(void)
-{
-#if defined(CONFIG_TEGRA_USE_SECURE_KERNEL)
-	return;
-#else
-	if (arch_timer_initialized) {
-		u32 tsc_ref_freq = tegra_clk_measure_input_freq();
-
-		/*
-		 * OSC detection function will bug out if revision is not QT and
-		 * the detected frequency is one of these two.
-		 */
-		if (tsc_ref_freq == 115200 || tsc_ref_freq == 230400)
-			tsc_ref_freq = 13000000;
-
-		/* Program CNTFRQ to the input frequency.
-		   NOTE: this is a write once (per CPU reset) register. */
-		__asm__("mcr p15, 0, %0, c14, c0, 0\n" : : "r" (tsc_ref_freq));
-	}
-#endif
-}
-
-static int arch_timer_cpu_notify(struct notifier_block *self,
-				    unsigned long action, void *data)
-{
-	switch (action) {
-	case CPU_STARTING:
-	case CPU_STARTING_FROZEN:
-		tegra_arch_timer_per_cpu_init();
-		break;
-	default:
-		break;
-	}
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block arch_timer_cpu_nb = {
-	.notifier_call = arch_timer_cpu_notify,
-};
-
-static int arch_timer_cpu_pm_notify(struct notifier_block *self,
-				    unsigned long action, void *data)
-{
-	switch (action) {
-	case CPU_PM_EXIT:
-		tegra_arch_timer_per_cpu_init();
-		break;
-	}
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block arch_timer_cpu_pm_nb = {
-	.notifier_call = arch_timer_cpu_pm_notify,
-};
-
-static int __init tegra_init_arch_timer(void)
-{
-	if (!local_timer_is_architected())
-		return -ENODEV;
-
-	clocksource_of_init();
-
-	register_cpu_notifier(&arch_timer_cpu_nb);
-	cpu_pm_register_notifier(&arch_timer_cpu_pm_nb);
-	arch_timer_initialized = true;
-	return 0;
-}
-
-static void __init tegra_init_late_timer(void)
-{}
-
-#ifdef CONFIG_PM_SLEEP
-
-static void __iomem *pmc = IO_ADDRESS(TEGRA_PMC_BASE);
-static u32 tsc_suspend_start;
-static u32 tsc_resume_start;
-
-#define pmc_writel(value, reg) \
-		writel(value, (u32)pmc + (reg))
-#define pmc_readl(reg) \
-		readl((u32)pmc + (reg))
-
-#define PMC_DPD_ENABLE			0x24
-#define PMC_DPD_ENABLE_TSC_MULT_ENABLE	(1 << 1)
-
-#define PMC_TSC_MULT			0x2b4
-#define PMC_TSC_MULT_FREQ_STS		(1 << 16)
-
-#define TSC_TIMEOUT_US			32
-
-void tegra_tsc_suspend(void)
-{
-	if (arch_timer_initialized) {
-		u32 reg = pmc_readl(PMC_DPD_ENABLE);
-		BUG_ON(reg & PMC_DPD_ENABLE_TSC_MULT_ENABLE);
-		reg |= PMC_DPD_ENABLE_TSC_MULT_ENABLE;
-		pmc_writel(reg, PMC_DPD_ENABLE);
-		tsc_suspend_start = timer_readl(TIMERUS_CNTR_1US);
-	}
-}
-
-void tegra_tsc_resume(void)
-{
-	if (arch_timer_initialized) {
-		u32 reg = pmc_readl(PMC_DPD_ENABLE);
-		BUG_ON(!(reg & PMC_DPD_ENABLE_TSC_MULT_ENABLE));
-		reg &= ~PMC_DPD_ENABLE_TSC_MULT_ENABLE;
-		pmc_writel(reg, PMC_DPD_ENABLE);
-		tsc_resume_start = timer_readl(TIMERUS_CNTR_1US);
-	}
-}
-
-void tegra_tsc_wait_for_suspend(void)
-{
-	if (arch_timer_initialized) {
-		while ((timer_readl(TIMERUS_CNTR_1US) - tsc_suspend_start) <
-			TSC_TIMEOUT_US) {
-			if (pmc_readl(PMC_TSC_MULT) & PMC_TSC_MULT_FREQ_STS)
-				break;
-			cpu_relax();
-		}
-	}
-}
-
-void tegra_tsc_wait_for_resume(void)
-{
-	if (arch_timer_initialized) {
-		while ((timer_readl(TIMERUS_CNTR_1US) - tsc_resume_start) <
-			TSC_TIMEOUT_US) {
-			if (!(pmc_readl(PMC_TSC_MULT) & PMC_TSC_MULT_FREQ_STS))
-				break;
-			cpu_relax();
-		}
-	}
-}
-
-int tegra_cpu_timer_get_remain(s64 *time)
-{
-	s32 cntp_tval;
-	int ret = 0;
-
-	asm volatile("mrc p15, 0, %0, c14, c2, 0" : "=r" (cntp_tval));
-
-	if (cntp_tval <= 0)
-		ret = -ETIME;
-	else
-		*time = (s64)((s64)cntp_tval * arch_timer_us_mult)
-			>> arch_timer_us_shift;
-
-	return ret;
-}
-
-#endif /* CONFIG_PM_SLEEP */
-
-#else  /* !CONFIG_ARM_ARCH_TIMER */
-static inline int tegra_init_arch_timer(void) { return -ENODEV; }
-static inline int tegra_init_late_arch_timer(void) { return -ENODEV; }
-#endif /* CONFIG_ARM_ARCH_TIMER */
-
-#ifdef CONFIG_RTC_CLASS
-/**
- * has_readtime - check rtc device has readtime ability
- * @dev: current device
- * @name_ptr: name to be returned
- *
- * This helper function checks to see if the rtc device can be
- * used for reading time
- */
-static int has_readtime(struct device *dev, void *name_ptr)
-{
-	struct rtc_device *candidate = to_rtc_device(dev);
-
-	if (!candidate->ops->read_time)
-		return 0;
-
-	return 1;
-}
-
-/**
- * tegra_get_linear_age - helper function to return linear age
- * from Jan 2012.
- *
- * @return
- * 1 - Jan 2012,
- * 2 - Feb 2012,
- * .....
- * 13 - Jan 2013
- */
-int tegra_get_linear_age(void)
-{
-	struct rtc_time tm;
-	int year, month, linear_age;
-	struct rtc_device *rtc_dev = NULL;
-	const char *name = NULL;
-	int ret;
-	struct device *dev = NULL;
-
-	linear_age = -1;
-	year = month = 0;
-	dev = class_find_device(rtc_class, NULL, &name, has_readtime);
-
-	if (!dev) {
-		pr_err("DVFS: No device with readtime capability\n");
-		goto done;
-	}
-
-	name = dev_name(dev);
-
-	pr_info("DVFS: Got RTC device name:%s\n", name);
-
-	if (name)
-		rtc_dev = rtc_class_open((char *)name);
-
-	if (!rtc_dev) {
-		pr_err("DVFS: No RTC device\n");
-		goto error_dev;
-	}
-
-	ret = rtc_read_time(rtc_dev, &tm);
-
-	if (ret < 0) {
-		pr_err("DVFS: Can't read RTC time\n");
-		goto error_rtc;
-	}
-
-	year = tm.tm_year;
-	/*Normalize it to 2012*/
-	year -= 112;
-	month = tm.tm_mon + 1;
-
-	if (year >= 0)
-		linear_age = year * 12 + month;
-
-error_rtc:
-	rtc_class_close(rtc_dev);
-error_dev:
-	put_device(dev);
-done:
-	return linear_age;
-
-}
-
-#else
-int tegra_get_linear_age()
-{
-	return -1;
-}
-#endif
+extern void __tegra_delay(unsigned long cycles);
+extern void __tegra_const_udelay(unsigned long loops);
+extern void __tegra_udelay(unsigned long usecs);
 
 void __init tegra_init_timer(void)
 {
@@ -850,8 +440,7 @@ void __init tegra_init_timer(void)
 
 	clk = clk_get_sys("timer", NULL);
 	if (IS_ERR(clk)) {
-		pr_warn("Unable to get timer clock."
-			" Assuming 12Mhz input clock.\n");
+		pr_warn("Unable to get timer clock. Assuming 12Mhz input clock.\n");
 		rate = 12000000;
 	} else {
 		tegra_clk_prepare_enable(clk);
@@ -931,10 +520,12 @@ void __init tegra_init_timer(void)
 	tegra_clockevent.irq = tegra_timer_irq.irq;
 	clockevents_register_device(&tegra_clockevent);
 
+#ifdef CONFIG_ARM_ARCH_TIMER
 	/* Architectural timers take precedence over broadcast timers.
 	   Only register a broadcast clockevent device if architectural
 	   timers do not exist or cannot be initialized. */
 	if (tegra_init_arch_timer())
+#endif
 		/* Architectural timers do not exist or cannot be initialzied.
 		   Fall back to using the broadcast timer as the sched clock. */
 		setup_sched_clock(tegra_read_sched_clock, 32, 1000000);
@@ -942,8 +533,10 @@ void __init tegra_init_timer(void)
 	register_syscore_ops(&tegra_timer_syscore_ops);
 
 	late_time_init = tegra_init_late_timer;
-}
 
-struct sys_timer tegra_timer = {
-	.init = tegra_init_timer,
-};
+	register_persistent_clock(NULL, tegra_read_persistent_clock);
+
+	//arm_delay_ops.delay		= __tegra_delay;
+	//arm_delay_ops.const_udelay	= __tegra_const_udelay;
+	//arm_delay_ops.udelay		= __tegra_udelay;
+}
